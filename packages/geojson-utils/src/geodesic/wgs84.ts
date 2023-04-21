83021c073251774d8eb27b0d0851d3fe9fc5886a
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-04-21 15:21:37 +0200

    Removed unused files

--- packages/geojson-utils/src/geodesic/wgs84.ts
+++
@@ -1,8 +0,0 @@
-import type {IGeodesic} from './types.js';
-
-import {geodesicParameters} from './utils.js';
-
-export const wgs84: IGeodesic = geodesicParameters({
-  a: 6378137.0,
-  f: 1.0 / 298.257223563,
-});
