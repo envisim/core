fb6a194b7e344b22416d65644a67de55aed0d0bc
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-04-25 20:57:05 +0200

    Updated parameters

--- packages/geosampling/src/sampleLinesOnAreas.ts
+++ packages/geosampling/src/sampleLinesOnAreas.ts
@@ -2,7 +2,6 @@ import {Random} from '@envisim/random';
 import {sampleBeltsOnAreas} from './sampleBeltsOnAreas.js';
 
 export type TsampleLinesOnAreasOpts = {
-  distBetween: number;
   rotation?: number;
   rand?: Random;
 };
@@ -11,21 +10,16 @@ export type TsampleLinesOnAreasOpts = {
  * Selects a sample of lines systematically over all areas.
  *
  * @param geoJSON - A GeoJSON FeatureCollection.
+ * @param distBetween - Distance in meters between the parallell lines.
  * @param opts - An options object.
- * @param opts.distBetween - Distance in meters between the parallell lines.
  * @param opts.rotation - Rotation angle in degrees.
  * @param opts.rand - Optional instance of Random.
  * @returns - A GeoJSON FeatureCollection of LineString/MultiLineString.
  */
 export const sampleLinesOnAreas = (
   geoJSON: GeoJSON.FeatureCollection,
+  distBetween: number,
   opts: TsampleLinesOnAreasOpts,
 ): GeoJSON.FeatureCollection => {
-  const beltOpts = {
-    distBetween: opts.distBetween || 100,
-    rotation: opts.rotation || 0,
-    halfWidth: 0,
-    rand: opts.rand ?? new Random(),
-  };
-  return sampleBeltsOnAreas(geoJSON, beltOpts);
+  return sampleBeltsOnAreas(geoJSON, distBetween, 0, opts);
 };
