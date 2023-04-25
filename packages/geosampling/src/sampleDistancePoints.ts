6eb15c25e0226cf80a1844c76a664316a95d6938
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-04-25 23:39:06 +0200

    updated parameters and types

--- packages/geosampling/src/sampleDistancePoints.ts
+++ packages/geosampling/src/sampleDistancePoints.ts
@@ -1,24 +1,12 @@
 import {Random} from '@envisim/random';
-import {samplePointsOnAreas} from './samplePointsOnAreas.js';
+import {
+  samplePointsOnAreas,
+  TsamplePointsOnAreasOpts,
+} from './samplePointsOnAreas.js';
 import {typeOfFrame} from './typeOfFrame.js';
 import {copy, distance} from '@envisim/geojson-utils';
 import {intersectPointAreaFeatures} from './intersectPointAreaFeatures.js';
-import {
-  uniformDetectionFunction,
-  halfNormalDetectionFunction,
-  effectiveRadius,
-} from './sampleDistanceUtils.js';
-
-export type TsampleDistancePointsOpts = {
-  cutoff: number;
-  df: 'uniform' | 'halfnormal';
-  sigma?: number;
-  method: 'uniform' | 'systematic';
-  sampleSize: number;
-  buffer?: number;
-  ratio?: number;
-  rand?: Random;
-};
+import {effectiveRadius} from './sampleDistanceUtils.js';
 
 /**
  * Distance sampling with points. Selects a point sample on an area frame
@@ -26,13 +14,12 @@ export type TsampleDistancePointsOpts = {
  * to (randomly) determine inclusion.
  *
  * @param frame - A GeoJSON FeatureCollection of Polygon/MultiPolygon features.
+ * @param method - The method to use "uniform" or "systematic".
+ * @param sampleSize - The expected sample size of points as integer > 0.
  * @param base - A GeoJSON FeatureCollection of single Point features.
- * @param opts - An object containing cutoff, df, sigma, method, sampleSize, buffer, ratio (dx/dy)
- * @param opts.cutoff - The maximum detection distance.
- * @param opts.df - The detection function "uniform" or "halfnormal".
- * @param opts.sigma - The sigma parameter for "halfnormal".
- * @param opts.method - The method to use "uniform" or "systematic"
- * @param opts.sampleSize - The expected sample size of points as integer > 0.
+ * @param detectionFunction - The detection function.
+ * @param cutoff - The maximum detection distance in meters.
+ * @param opts - An object containing buffer, ratio (dx/dy), rand.
  * @param opts.buffer - Optional buffer in meters (default cutoff).
  * @param opts.ratio - The ratio (dx/dy) for systematic sampling (default 1).
  * @param opts.rand - An optional instance of Random.
@@ -40,8 +27,12 @@ export type TsampleDistancePointsOpts = {
  */
 export const sampleDistancePoints = (
   frame: GeoJSON.FeatureCollection,
+  method: 'uniform' | 'systematic',
+  sampleSize: number,
   base: GeoJSON.FeatureCollection,
-  opts: TsampleDistancePointsOpts,
+  detectionFunction: Function,
+  cutoff: number,
+  opts: TsamplePointsOnAreasOpts,
 ): GeoJSON.FeatureCollection => {
   // Check types first
   const frameType = typeOfFrame(frame);
@@ -54,24 +45,15 @@ export const sampleDistancePoints = (
   if (baseType !== 'point') {
     throw new Error('Parameter base must be a FeatureCollection of Points.');
   }
-  const cutoff = opts.cutoff ?? 10;
-  // Construct detection function
-  let g: Function;
-  if (opts.df === 'halfnormal') {
-    g = halfNormalDetectionFunction(opts.sigma ?? 1, cutoff);
-  } else {
-    // default to uniform
-    g = uniformDetectionFunction(cutoff);
-  }
   // Compute effective radius
-  const effRadius = effectiveRadius(g, cutoff);
+  const effRadius = effectiveRadius(detectionFunction, cutoff);
   // Set random generator
   const rand = opts.rand ?? new Random();
   opts.rand = rand;
   // Select sample of points (optional buffer via opts)
   const buffer = opts.buffer ?? cutoff;
   opts.buffer = buffer;
-  const pointSample = samplePointsOnAreas(frame, opts);
+  const pointSample = samplePointsOnAreas(frame, method, sampleSize, opts);
   // To store sampled features
   const sampledFeatures: GeoJSON.Feature[] = [];
 
@@ -87,7 +69,7 @@ export const sampleDistancePoints = (
             basePointCoords,
             samplePoint.geometry.coordinates,
           );
-          if (dist < cutoff && rand.float() < g(dist)) {
+          if (dist < cutoff && rand.float() < detectionFunction(dist)) {
             // Check if base point exists in this frame (frame could be part/stratum)
             for (let i = 0; i < frame.features.length; i++) {
               const frameFeature = frame.features[i];
