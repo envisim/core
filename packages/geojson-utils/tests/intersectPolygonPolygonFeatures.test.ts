0aa022230e7bf23acc113b1f3850a9ba10c8705d
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-04-28 16:07:38 +0200

    Update intersectPolygonPolygonFeatures.test.ts

--- packages/geojson-utils/tests/intersectPolygonPolygonFeatures.test.ts
+++ packages/geojson-utils/tests/intersectPolygonPolygonFeatures.test.ts
@@ -1,5 +1,5 @@
 import {intersectPolygonPolygonFeatures} from '../src/intersectPolygonPolygonFeatures.js';
-
+import {area} from '../src/area.js';
 describe('intersectPolygonPolygonFeatures', () => {
   // polygon
   const polygon1: GeoJSON.Feature = {
@@ -34,9 +34,29 @@ describe('intersectPolygonPolygonFeatures', () => {
     },
     properties: {},
   };
+  const polygon3: GeoJSON.Feature = {
+    type: 'Feature',
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [0, 0],
+          [0.5, 0],
+          [0.5, 0.5],
+          [0, 0.5],
+          [0, 0],
+        ],
+      ],
+    },
+    properties: {},
+  };
   const intersect = intersectPolygonPolygonFeatures(polygon1, polygon2);
+
   //console.log(JSON.stringify(intersect, null, 2));
   test('intersectPolygonPolygonFeatures', () => {
-    //expect(area(polygon)).toBeCloseTo(13662703680020.13, 1);
+    if (!intersect.geoJSON) {
+      throw new Error('No intersect.');
+    }
+    expect(area(intersect.geoJSON)).toBeCloseTo(area(polygon3), 4);
   });
 });
