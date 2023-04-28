29251a0e841acd40fa3e7839e50742c7a8e08da8
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-04-28 11:42:02 +0200

    Create asFeatureCollection.ts

---
+++ packages/geojson-utils/src/asFeatureCollection.ts
@@ -0,0 +1,38 @@
+import {copy as fnCopy} from './copy.js';
+
+/**
+ * Builds a new FeatureCollection from any GeoJSON type. No overhead. Internal.
+ * @param geoJSON - Any GeoJSON.
+ * @param copy - Boolean, default false. If true, then makes a copy of input.
+ * @returns - A GeoJSON FeatureCollection.
+ */
+export const asFeatureCollection = (
+  geoJSON: GeoJSON.GeoJSON,
+  copy: boolean = false,
+): GeoJSON.FeatureCollection => {
+  let g: GeoJSON.GeoJSON;
+  if (copy) {
+    g = fnCopy(geoJSON);
+  } else {
+    g = geoJSON;
+  }
+  switch (g.type) {
+    case 'FeatureCollection':
+      return g;
+    case 'Feature':
+      return {type: 'FeatureCollection', features: [g]};
+    case 'Point':
+    case 'MultiPoint':
+    case 'LineString':
+    case 'MultiLineString':
+    case 'Polygon':
+    case 'MultiPolygon':
+    case 'GeometryCollection':
+      return {
+        type: 'FeatureCollection',
+        features: [{type: 'Feature', geometry: g, properties: {}}],
+      };
+    default:
+      throw new Error('Only GeoJSON types are supported.');
+  }
+};
