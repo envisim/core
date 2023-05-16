64d5eb8b719cc16b18c5ad83b04dbafe5d40343f
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-05-16 10:10:15 +0200

    Rewrite using new classes

--- packages/geojson-utils/src/intersectLineLineFeatures.ts
+++ packages/geojson-utils/src/intersectLineLineFeatures.ts
@@ -1,16 +1,17 @@
-import {bbox, bboxInBbox} from './bbox.js';
+import {bboxInBbox} from './bbox.js';
 import {intersectSegments} from './intersectSegments.js';
-import {toPoint, toMultiPoint} from './to.js';
 import type * as GJ from './geojson/types.js';
+import {LineFeature} from './geojson/lines/ClassLineFeature.js';
+import {Point} from './geojson/points/ClassPoint.js';
+import {MultiPoint} from './geojson/points/ClassMultiPoint.js';
+import {PointFeature} from './geojson/points/ClassPointFeature.js';
 
 interface Intersect {
   geoJSON?: GJ.PointFeature;
 }
 
-// TODO: Fix LineGeometryCollection
-
 /**
- * Computes the intersect of features containing LineString/MultiLineString as
+ * Computes the intersect of two LineFeatures as
  * the crossing-points between the lines in the two features.
  * Returns an empty object {} if no crossings and {geoJSON}
  * if intersection. Then geoJSON is a PointFeature.
@@ -23,42 +24,41 @@ export const intersectLineLineFeatures = (
   firstFeature: GJ.LineFeature,
   secondFeature: GJ.LineFeature,
 ): Intersect => {
-  const g1 = firstFeature.geometry;
-  const g2 = secondFeature.geometry;
-  if (g1.type !== 'LineString' && g1.type !== 'MultiLineString') {
-    throw new Error(
-      'Geometry type for firstFeature must be LineString or MultiLineString.',
-    );
-  }
-  if (g2.type !== 'LineString' && g2.type !== 'MultiLineString') {
-    throw new Error(
-      'Geometry type for secondFeature must be LineString or MultiLineString.',
-    );
-  }
+  const f1 = LineFeature.isFeature(firstFeature)
+    ? firstFeature
+    : new LineFeature(firstFeature);
+  const f2 = LineFeature.isFeature(secondFeature)
+    ? secondFeature
+    : new LineFeature(secondFeature);
+
   const points: GJ.Position[] = [];
-  const box1 = firstFeature.bbox || bbox(firstFeature);
-  const box2 = secondFeature.bbox || bbox(secondFeature);
+  const box1 = f1.getBBox();
+  const box2 = f2.getBBox();
 
   if (bboxInBbox(box1, box2)) {
-    const coords1 =
-      g1.type === 'LineString' ? [g1.coordinates] : g1.coordinates;
-    const coords2 =
-      g2.type === 'LineString' ? [g2.coordinates] : g2.coordinates;
-    // Now we have coordinates for two MultiLineStrings.
-    // Go thru all segments to find possible intersection points.
-    coords1.forEach((line1) => {
-      coords2.forEach((line2) => {
-        for (let i = 0; i < line1.length - 1; i++) {
-          for (let j = 0; j < line2.length - 1; j++) {
-            const intersect = intersectSegments(
-              [line1[i], line1[i + 1]],
-              [line2[j], line2[j + 1]],
-            );
-            if (intersect.point) {
-              points.push(intersect.point);
+    f1.geomEach((g1: GJ.LineObject) => {
+      f2.geomEach((g2: GJ.LineObject) => {
+        const coords1 =
+          g1.type === 'LineString' ? [g1.coordinates] : g1.coordinates;
+        const coords2 =
+          g2.type === 'LineString' ? [g2.coordinates] : g2.coordinates;
+        // Now we have coordinates for two MultiLineStrings.
+        // Go thru all segments to find possible intersection points.
+        coords1.forEach((line1) => {
+          coords2.forEach((line2) => {
+            for (let i = 0; i < line1.length - 1; i++) {
+              for (let j = 0; j < line2.length - 1; j++) {
+                const intersect = intersectSegments(
+                  [line1[i], line1[i + 1]],
+                  [line2[j], line2[j + 1]],
+                );
+                if (intersect.point) {
+                  points.push(intersect.point);
+                }
+              }
             }
-          }
-        }
+          });
+        });
       });
     });
   }
@@ -67,10 +67,10 @@ export const intersectLineLineFeatures = (
   }
   if (points.length === 1) {
     return {
-      geoJSON: toPoint(points[0]),
+      geoJSON: PointFeature.create(Point.create(points[0]), {}),
     };
   }
   return {
-    geoJSON: toMultiPoint(points),
+    geoJSON: PointFeature.create(MultiPoint.create(points), {}),
   };
 };
