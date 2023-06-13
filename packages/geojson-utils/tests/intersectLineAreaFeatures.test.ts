50b95563c6c8ffd48bc84a62c0a6638019ceb05c
wpmg <wilmer.prentius@slu.se>
2023-06-13 23:10:59 +0200

    Fixed it

--- packages/geojson-utils/tests/intersectLineAreaFeatures.test.ts
+++ packages/geojson-utils/tests/intersectLineAreaFeatures.test.ts
@@ -5,7 +5,6 @@ import {
   intersectLineAreaFeatures,
   GeoJSON,
 } from '../src/index.js';
-import {intersectLineAreaFeatures as iLAF} from '../src/intersectLineAreaFeatures2.js';
 import './_equalArrays.testf';
 
 describe('intersectLinePolygonFeatures', () => {
@@ -18,79 +17,13 @@ describe('intersectLinePolygonFeatures', () => {
       [-0.5, -0.5],
     ],
   ];
-  const linecoords: GeoJSON.Position[] = [
-    [-2, 0],
-    [-0.5, 0],
-    [0, 0.1],
-    [0.1, 0],
-    [0.4, 0],
-    [0.5, 0],
-    [2, 0],
-  ];
-  const line = LineFeature.create(
-    {
-      type: 'LineString',
-      coordinates: linecoords,
-    },
-    {},
-    true,
-  );
-
-  test('Simple polygon', () => {
-    const polygon = AreaFeature.create({
-      type: 'Polygon',
-      coordinates: polycoords,
-    });
-
-    const intersection = intersectLineAreaFeatures(
-      line,
-      polygon,
-    ) as LineFeature;
-    expect(intersection).not.toBeNull();
-
-    expect(LineString.isObject(intersection.geometry)).toBe(true);
-    const coords = (intersection.geometry as LineString).coordinates;
-
-    expect(coords.length).toBe(5);
-    expect(coords[0]).arrayToAlmostEqual(linecoords[1], 1e-9);
-    expect(coords[1]).arrayToAlmostEqual(linecoords[2], 1e-9);
-    expect(coords[2]).arrayToAlmostEqual(linecoords[3], 1e-9);
-    expect(coords[3]).arrayToAlmostEqual(linecoords[4], 1e-9);
-    expect(coords[4]).arrayToAlmostEqual(linecoords[5], 1e-9);
-  });
-
-  test('Overlapping multipolygon', () => {
-    const polygon = AreaFeature.create({
-      type: 'MultiPolygon',
-      coordinates: [polycoords, polycoords],
-    });
-
-    const intersection = intersectLineAreaFeatures(
-      line,
-      polygon,
-    ) as LineFeature;
-    expect(intersection).not.toBeNull();
-
-    expect(LineString.isObject(intersection.geometry)).toBe(true);
-    const coords = (intersection.geometry as LineString).coordinates;
-
-    expect(coords.length).toBe(5);
-    expect(coords[0]).arrayToAlmostEqual(linecoords[1], 1e-9);
-    expect(coords[1]).arrayToAlmostEqual(linecoords[2], 1e-9);
-    expect(coords[2]).arrayToAlmostEqual(linecoords[3], 1e-9);
-    expect(coords[3]).arrayToAlmostEqual(linecoords[4], 1e-9);
-    expect(coords[4]).arrayToAlmostEqual(linecoords[5], 1e-9);
-  });
-});
-
-describe('intersectLinePolygonFeatures 2', () => {
-  const polycoords: GeoJSON.Position[][] = [
+  const polycoords2: GeoJSON.Position[][] = [
     [
-      [-0.5, -0.5],
-      [0.5, -0.5],
-      [0.5, 0.5],
-      [-0.5, 0.5],
-      [-0.5, -0.5],
+      [0.0, -1.0],
+      [0.0, 1.0],
+      [1.0, 1.0],
+      [1.0, -1.0],
+      [0.0, -1.0],
     ],
   ];
   const linecoords: GeoJSON.Position[] = [
@@ -117,7 +50,10 @@ describe('intersectLinePolygonFeatures 2', () => {
       coordinates: polycoords,
     });
 
-    const intersection = iLAF(line, polygon) as LineFeature;
+    const intersection = intersectLineAreaFeatures(
+      line,
+      polygon,
+    ) as LineFeature;
     expect(intersection).not.toBeNull();
 
     expect(LineString.isObject(intersection.geometry)).toBe(true);
@@ -137,7 +73,10 @@ describe('intersectLinePolygonFeatures 2', () => {
       coordinates: [polycoords, polycoords],
     });
 
-    const intersection = iLAF(line, polygon) as LineFeature;
+    const intersection = intersectLineAreaFeatures(
+      line,
+      polygon,
+    ) as LineFeature;
     expect(intersection).not.toBeNull();
 
     expect(LineString.isObject(intersection.geometry)).toBe(true);
@@ -149,5 +88,29 @@ describe('intersectLinePolygonFeatures 2', () => {
     expect(coords[2]).arrayToAlmostEqual(linecoords[3], 1e-9);
     expect(coords[3]).arrayToAlmostEqual(linecoords[4], 1e-9);
     expect(coords[4]).arrayToAlmostEqual(linecoords[5], 1e-9);
+  });
+
+  test('Three multipolygons', () => {
+    const polygon = AreaFeature.create({
+      type: 'MultiPolygon',
+      coordinates: [polycoords, polycoords, polycoords2],
+    });
+
+    const intersection = intersectLineAreaFeatures(
+      line,
+      polygon,
+    ) as LineFeature;
+    expect(intersection).not.toBeNull();
+
+    expect(LineString.isObject(intersection.geometry)).toBe(true);
+    const coords = (intersection.geometry as LineString).coordinates;
+
+    expect(coords.length).toBe(6);
+    expect(coords[0]).arrayToAlmostEqual(linecoords[1], 1e-9);
+    expect(coords[1]).arrayToAlmostEqual(linecoords[2], 1e-9);
+    expect(coords[2]).arrayToAlmostEqual(linecoords[3], 1e-9);
+    expect(coords[3]).arrayToAlmostEqual(linecoords[4], 1e-9);
+    expect(coords[4]).arrayToAlmostEqual(linecoords[5], 1e-9);
+    expect(coords[5]).arrayToAlmostEqual([1.0, 0.0], 1e-9);
   });
 });
