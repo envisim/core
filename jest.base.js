4c0f5bee4269d0509f6b1f0026774f0cb1f5cdee
wpmg <wilmer.prentius@slu.se>
2023-06-14 16:22:35 +0200

    Fixed jest

--- jest.base.js
+++ jest.base.js
@@ -1,7 +1,17 @@
 export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: 'tsconfig.jest.json',
+      },
+    ],
+  },
   moduleNameMapper: {
     '(.+)\\.[jt]s': '$1',
   },
   roots: ['<rootDir>/tests/'],
+  extensionsToTreatAsEsm: ['.ts'],
 };
