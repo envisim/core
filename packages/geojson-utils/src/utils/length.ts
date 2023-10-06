92f585f908b7ea8a9aaa34c523b0cddc365ab0e0
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-10-06 10:06:37 +0200

    Update length.ts
    Updated computation of length to add points in case segments are longer than 1000 meters. Also switched to geodesic distance as that were a better match than rhumb distance.

--- packages/geojson-utils/src/utils/length.ts
+++ packages/geojson-utils/src/utils/length.ts
@@ -1,27 +1,47 @@
 import type * as GJ from '../types/geojson.js';
-import {rhumbDistance} from '../index.js';
+import {distance} from '../index.js';
 
 /**
- * Computes the length of a GeoJSON segment, where the segment is a
- * rhumb line.
- * @param start start point [lon,lat]
- * @param end end point [lon,lat]
+ * Computes the approximate length of a GeoJSON segment, where the segment is
+ * of type plate carré.
+ * @param p1 start point [lon,lat]
+ * @param p2 end point [lon,lat]
  * @returns the length of the segment in meters.
  */
-export function lengthOfSegment(start: GJ.Position, end: GJ.Position): number {
-  return rhumbDistance(start, end);
+export function lengthOfSegment(p1: GJ.Position, p2: GJ.Position): number {
+  const distToEnd = distance(p1, p2);
+  const dist = 1000;
+  if (distToEnd > dist) {
+    let L = 0; // Aggregate length for segments longer than dist
+    const numPointsToAdd = Math.ceil(distToEnd / dist);
+    const [lon0, lat0] = p1;
+    const [lon1, lat1] = p2;
+    let prev: GJ.Position = [lon0, lat0];
+
+    for (let i = 1; i <= numPointsToAdd; i++) {
+      const t = i / numPointsToAdd;
+      const lon = lon0 + t * (lon1 - lon0);
+      const lat = lat0 + t * (lat1 - lat0);
+      L += distance(prev, [lon, lat]);
+      prev = [lon, lat];
+    }
+
+    return L;
+  }
+
+  return distToEnd;
 }
 
 /**
- * Computes the length of a LineString, where each segment is a
- * rhumb line.
+ * Computes the approximate length of a LineString, where each segment is
+ * of type plate carrée.
  * @param ls Coordinates of a LineString
  * @returns the length of the LineString in meters.
  */
 export function lengthOfLineString(ls: GJ.Position[]) {
   let l = 0;
   for (let i = 0; i < ls.length - 1; i++) {
-    l += rhumbDistance(ls[i], ls[i + 1]);
+    l += lengthOfSegment(ls[i], ls[i + 1]);
   }
   return l;
 }
