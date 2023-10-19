73ab5f49e3b947f820e25f4e2d476372650f0ac7
AntonGrafstrom <anton.grafstrom@gmail.com>
2023-10-19 16:14:53 +0200

    Commented out the files in sampling

--- packages/geosampling/src/sampling/types.ts
+++ packages/geosampling/src/sampling/types.ts
@@ -1,30 +1,30 @@
-import type {IPropertyRecord} from '@envisim/geojson-utils';
-import type {Random} from '@envisim/random';
+// import type {IPropertyRecord} from '@envisim/geojson-utils';
+// import type {Random} from '@envisim/random';
 
-export interface ISampleStratification {
-  collection: GeoJSON.FeatureCollection;
-  validProps: IPropertyRecord;
-}
+// export interface ISampleStratification {
+//   collection: GeoJSON.FeatureCollection;
+//   validProps: IPropertyRecord;
+// }
 
-interface ISampleOptionsBase {
-  rand: Random;
-  sampleSize: number;
-  probabilitiesFrom?: string | null;
-}
+// interface ISampleOptionsBase {
+//   rand: Random;
+//   sampleSize: number;
+//   probabilitiesFrom?: string | null;
+// }
 
-export interface ISampleOptionsFinite extends ISampleOptionsBase {
-  spreadOn?: string[];
-  balanceOn?: string[];
-}
+// export interface ISampleOptionsFinite extends ISampleOptionsBase {
+//   spreadOn?: string[];
+//   balanceOn?: string[];
+// }
 
-export interface ISampleOptionsContinuous extends ISampleOptionsBase {
-  shape?: GeoJSON.Feature;
-  shapeRotationRandom?: boolean;
-}
+// export interface ISampleOptionsContinuous extends ISampleOptionsBase {
+//   shape?: GeoJSON.Feature;
+//   shapeRotationRandom?: boolean;
+// }
 
-export interface IPropsStratification {
-  /** The {@link ILayer.propCategories} to perform the stratification on. */
-  stratify: string;
-  /** An array with expected sample size for each value defined on the {@link ILayerCategory}. */
-  sampleSize: number[];
-}
+// export interface IPropsStratification {
+//   /** The {@link ILayer.propCategories} to perform the stratification on. */
+//   stratify: string;
+//   /** An array with expected sample size for each value defined on the {@link ILayerCategory}. */
+//   sampleSize: number[];
+// }
