d2536bfef284bc3e692cfc60f0c4d1986c9dfbd7
AntonGrafstrom <anton.grafstrom@gmail.com>
2024-03-04 13:30:29 +0100

    Update antimeridian.test.ts
    Removed console.log

--- packages/geojson-utils/tests/antimeridian.test.ts
+++ packages/geojson-utils/tests/antimeridian.test.ts
@@ -19,14 +19,11 @@ describe('antimeridian', () => {
     ],
   ];
 
-  console.log(
-    JSON.stringify(cutLineGeometry({type: 'LineString', coordinates: ls})),
-  );
-  console.log(
-    JSON.stringify(cutAreaGeometry({type: 'Polygon', coordinates: pol})),
-  );
+  const cutLine = cutLineGeometry({type: 'LineString', coordinates: ls});
+  const cutPol = cutAreaGeometry({type: 'Polygon', coordinates: pol});
 
   test('antimeridian', () => {
-    //expect(area0).toBeCloseTo(area, 1);
+    expect(cutLine.type).toBe('MultiLineString');
+    expect(cutPol.type).toBe('MultiPolygon');
   });
 });
