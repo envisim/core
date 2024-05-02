8ff7fa17286d9dceb03db28465a89299ef30a9db
AntonGrafstrom <anton.grafstrom@gmail.com>
2024-05-02 10:20:18 +0200

    Update index.ts
    Added exports

--- packages/envisim-geosampling/src/index.ts
+++ packages/envisim-geosampling/src/index.ts
@@ -29,11 +29,14 @@ export {typeOfFeature} from './typeOfFeature.js';
 
 // Model features/tracts
 export {
+  radiusOfModelFeature,
+  sizeOfModelFeature,
   straightLineFeature,
   ellLineFeature,
   rectangularLineFeature,
   squareLineFeature,
   regularPolygonLineFeature,
+  regularPolygonPointFeature,
   circleLineFeature,
   circleAreaFeature,
   squareCircleAreaFeature,
