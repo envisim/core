d0d5b61e545df337e7878c8c3c493eb25c5a51f3
AntonGrafstrom <anton.grafstrom@gmail.com>
2024-05-29 17:02:43 +0200

    Update sampleContinuous.ts
    First attempt on sample continuous

--- packages/envisim-geosampling/src/sampling/sampleContinuous.ts
+++ packages/envisim-geosampling/src/sampling/sampleContinuous.ts
@@ -1,84 +1,137 @@
-// import {
-//   createCollection,
-//   filterCollectionByPropertyValue,
-//   IPropertyRecord,
-//   mergeCollections,
-//   TCollectionType,
-// } from '@envisim/geojson-utils';
+import {
+  AreaCollection,
+  type GeoJSON as GJ,
+  Layer,
+  LineCollection,
+  PointCollection,
+} from '@envisim/geojson-utils';
+import {type Random} from '@envisim/random';
 
-// import {sampleFromLine, sampleFromPolygon} from './sampleFromGeoJson.js';
-// import type {IPropsStratification, ISampleOptionsContinuous} from './types.js';
+import {
+  type TsampleTractsOnAreasOpts,
+  sampleFeaturesOnAreas,
+} from '../sampleFeaturesOnAreas.js';
+import {samplePointsOnLines} from '../samplePointsOnLines.js';
+import {
+  type TsampleBeltsOnAreasOpts,
+  sampleSystematicBeltsOnAreas,
+} from '../sampleSystematicBeltsOnAreas.js';
+import {
+  type TsampleLinesOnAreasOpts,
+  sampleSystematicLinesOnAreas,
+} from '../sampleSystematicLinesOnAreas.js';
 
-// export function sampleContinuous(
-//   geojsonType: Exclude<TCollectionType, 'point'>,
-//   methodName: string,
-//   collection: GeoJSON.FeatureCollection,
-//   sampleOptions: ISampleOptionsContinuous,
-//   validProps: IPropertyRecord = {},
-// ): GeoJSON.FeatureCollection {
-//   if (sampleOptions.sampleSize === 0) {
-//     return createCollection();
-//   }
+interface ISampleAreaFeaturesOnAreasOpts extends TsampleTractsOnAreasOpts {
+  modelFeature: GJ.AreaFeature;
+  design: 'uniform' | 'systematic';
+  sampleSize: number;
+  methodName: 'areaFeature';
+}
 
-//   switch (geojsonType) {
-//     case 'line':
-//       return sampleFromLine(methodName, collection, sampleOptions, validProps);
-//     case 'polygon':
-//       return sampleFromPolygon(
-//         methodName,
-//         collection,
-//         sampleOptions,
-//         validProps,
-//       );
-//     default:
-//       throw new Error('geojsonType does not match any allowed values');
-//   }
-// }
+interface ISampleBeltsOnAreasOpts extends TsampleBeltsOnAreasOpts {
+  distBetween: number;
+  halfWidth: number;
+  methodName: 'belt';
+}
 
-// export function sampleContinuousStratified(
-//   geojsonType: Exclude<TCollectionType, 'point'>,
-//   methodName: string,
-//   collection: GeoJSON.FeatureCollection,
-//   sampleOptions: ISampleOptionsContinuous,
-//   validProps: IPropertyRecord,
-//   stratification: IPropsStratification,
-// ): GeoJSON.FeatureCollection {
-//   if (!(stratification.stratify in validProps))
-//     throw new Error(
-//       'stratification is not possible as validProps does not contain the required property',
-//     );
+export type TsampleAreaToAreaOpts =
+  | ISampleAreaFeaturesOnAreasOpts
+  | ISampleBeltsOnAreasOpts;
 
-//   // The ID of the property
-//   const property = stratification.stratify;
-//   // The current property object
-//   const propertyObj = validProps[property];
+export function sampleAreaToArea(
+  layer: Layer<AreaCollection>,
+  opts: TsampleAreaToAreaOpts,
+): Layer<AreaCollection> {
+  if (opts.methodName === 'belt') {
+    return sampleSystematicBeltsOnAreas(
+      layer,
+      opts.distBetween,
+      opts.halfWidth,
+      opts,
+    );
+  }
+  if (opts.methodName == 'areaFeature') {
+    return sampleFeaturesOnAreas(
+      layer,
+      opts.design,
+      opts.sampleSize,
+      opts.modelFeature,
+      opts,
+    );
+  }
+  throw new Error(
+    'methodName does not match available methods belt or areaFeature',
+  );
+}
 
-//   // We only allow stratification on categorical variables
-//   if (propertyObj.type !== 'categorical')
-//     throw new Error(
-//       'stratification is only possible to perform on categorical properties',
-//     );
+interface ISampleLinesOnAreasOpts extends TsampleLinesOnAreasOpts {
+  distBetween: number;
+  methodName: 'line';
+}
 
-//   const sampleSizeFallback: number =
-//     sampleOptions.sampleSize / propertyObj.values.length;
+interface ISampleLineFeaturesOnAreasOpts extends TsampleTractsOnAreasOpts {
+  modelFeature: GJ.LineFeature;
+  design: 'uniform' | 'systematic';
+  sampleSize: number;
+  methodName: 'lineFeature';
+}
 
-//   /*
-//    * For each value of the property, run sampleFinite.
-//    * If sampleSize is not defined on stratification, use sampleSizeFallback.
-//    * Merge the returning FeatureCollections
-//    */
-//   return mergeCollections(
-//     ...propertyObj.values.map((e, i) =>
-//       sampleContinuous(
-//         geojsonType,
-//         methodName,
-//         filterCollectionByPropertyValue(collection, property, e),
-//         {
-//           ...sampleOptions,
-//           sampleSize: stratification.sampleSize[i] ?? sampleSizeFallback,
-//         },
-//         validProps,
-//       ),
-//     ),
-//   );
-// }
+export type TsampleAreaToLineOpts =
+  | ISampleLineFeaturesOnAreasOpts
+  | ISampleLinesOnAreasOpts;
+
+export function sampleAreaToLine(
+  layer: Layer<AreaCollection>,
+  opts: TsampleAreaToLineOpts,
+): Layer<LineCollection> {
+  if (opts.methodName === 'line') {
+    return sampleSystematicLinesOnAreas(layer, opts.distBetween, opts);
+  }
+  if (opts.methodName == 'lineFeature') {
+    return sampleFeaturesOnAreas(
+      layer,
+      opts.design,
+      opts.sampleSize,
+      opts.modelFeature,
+      opts,
+    );
+  }
+  throw new Error(
+    'methodName does not match available methods line or lineFeature',
+  );
+}
+
+interface ISamplePointFeaturesOnAreasOpts extends TsampleTractsOnAreasOpts {
+  modelFeature: GJ.PointFeature;
+  design: 'uniform' | 'systematic';
+  sampleSize: number;
+}
+export type TsampleAreaToPointOpts = ISamplePointFeaturesOnAreasOpts;
+
+export function sampleAreaToPoint(
+  layer: Layer<AreaCollection>,
+  opts: TsampleAreaToPointOpts,
+): Layer<PointCollection> {
+  return sampleFeaturesOnAreas(
+    layer,
+    opts.design,
+    opts.sampleSize,
+    opts.modelFeature,
+    opts,
+  );
+}
+
+export type TsampleLineToPointOpts = {
+  design: 'uniform' | 'systematic';
+  sampleSize: number;
+  rand?: Random;
+};
+
+export function sampleLineToPoint(
+  layer: Layer<LineCollection>,
+  opts: TsampleLineToPointOpts,
+): Layer<PointCollection> {
+  return samplePointsOnLines(layer, opts.design, opts.sampleSize, opts);
+}
+
+// TODO: Add support for stratification for each method
