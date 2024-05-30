c6645c6129330fe095ff0dcec3780cfac2600c8a
AntonGrafstrom <anton.grafstrom@gmail.com>
2024-05-30 10:43:21 +0200

    Update sampleContinuous.ts
    Added support for stratification.

--- packages/envisim-geosampling/src/sampling/sampleContinuous.ts
+++ packages/envisim-geosampling/src/sampling/sampleContinuous.ts
@@ -1,11 +1,15 @@
 import {
   AreaCollection,
+  AreaFeature,
   type GeoJSON as GJ,
   Layer,
   LineCollection,
+  LineFeature,
   PointCollection,
+  PointFeature,
 } from '@envisim/geojson-utils';
 import {type Random} from '@envisim/random';
+import {copy} from '@envisim/utils';
 
 import {
   type TsampleTractsOnAreasOpts,
@@ -31,7 +35,7 @@ interface ISampleAreaFeaturesOnAreasOpts extends TsampleTractsOnAreasOpts {
 interface ISampleBeltsOnAreasOpts extends TsampleBeltsOnAreasOpts {
   distBetween: number;
   halfWidth: number;
-  methodName: 'belt';
+  methodName: 'systematicBelt';
 }
 
 export type TsampleAreaToAreaOpts =
@@ -42,7 +46,7 @@ export function sampleAreaToArea(
   layer: Layer<AreaCollection>,
   opts: TsampleAreaToAreaOpts,
 ): Layer<AreaCollection> {
-  if (opts.methodName === 'belt') {
+  if (opts.methodName === 'systematicBelt') {
     return sampleSystematicBeltsOnAreas(
       layer,
       opts.distBetween,
@@ -66,7 +70,7 @@ export function sampleAreaToArea(
 
 interface ISampleLinesOnAreasOpts extends TsampleLinesOnAreasOpts {
   distBetween: number;
-  methodName: 'line';
+  methodName: 'systematicLine';
 }
 
 interface ISampleLineFeaturesOnAreasOpts extends TsampleTractsOnAreasOpts {
@@ -84,7 +88,7 @@ export function sampleAreaToLine(
   layer: Layer<AreaCollection>,
   opts: TsampleAreaToLineOpts,
 ): Layer<LineCollection> {
-  if (opts.methodName === 'line') {
+  if (opts.methodName === 'systematicLine') {
     return sampleSystematicLinesOnAreas(layer, opts.distBetween, opts);
   }
   if (opts.methodName == 'lineFeature') {
@@ -97,7 +101,7 @@ export function sampleAreaToLine(
     );
   }
   throw new Error(
-    'methodName does not match available methods line or lineFeature',
+    'methodName does not match available methods systematicLine or lineFeature',
   );
 }
 
@@ -134,4 +138,254 @@ export function sampleLineToPoint(
   return samplePointsOnLines(layer, opts.design, opts.sampleSize, opts);
 }
 
-// TODO: Add support for stratification for each method
+// Stratification for each method.
+
+export function sampleLineToPointStratified(
+  layer: Layer<LineCollection>,
+  stratifyOn: string,
+  opts: TsampleLineToPointOpts | TsampleLineToPointOpts[],
+): Layer<PointCollection> {
+  if (!(stratifyOn in layer.propertyRecord))
+    throw new Error(
+      'stratification is not possible as property record does not contain the required property',
+    );
+
+  // The current property object.
+  const propertyObj = layer.propertyRecord[stratifyOn];
+
+  // We only allow stratification on categorical variables.
+  if (propertyObj.type !== 'categorical')
+    throw new Error(
+      'stratification is only possible to perform on categorical properties',
+    );
+
+  // Make an array of opts if it is not provided as an array.
+  const fixedOpts = Array.isArray(opts)
+    ? opts
+    : new Array(propertyObj.values.length).fill(opts);
+
+  // Make sure it is of correct length.
+  if (fixedOpts.length !== propertyObj.values.length) {
+    throw new Error(
+      'length of provided opts array does not match number of strata',
+    );
+  }
+
+  /*
+   * For each value of the property, run sampleLineToPoint.
+   * Merge the returning features into a single array.
+   */
+  let features: PointFeature[] = [];
+
+  propertyObj.values.forEach((_v, i) => {
+    const stratumFeatures = layer.collection.features.filter(
+      (f) => f.properties[stratifyOn] === i,
+    );
+
+    const stratumLayer = new Layer(
+      new LineCollection({features: stratumFeatures}, true),
+      layer.propertyRecord,
+      true,
+    );
+
+    const sampledFeatures = sampleLineToPoint(stratumLayer, fixedOpts[i])
+      .collection.features;
+
+    features = [...features, ...sampledFeatures];
+  });
+
+  return new Layer(
+    new PointCollection({features}, true),
+    copy(layer.propertyRecord),
+    true,
+  );
+}
+
+export function sampleAreaToPointStratified(
+  layer: Layer<AreaCollection>,
+  stratifyOn: string,
+  opts: TsampleAreaToPointOpts | TsampleAreaToPointOpts[],
+): Layer<PointCollection> {
+  if (!(stratifyOn in layer.propertyRecord))
+    throw new Error(
+      'stratification is not possible as property record does not contain the required property',
+    );
+
+  // The current property object.
+  const propertyObj = layer.propertyRecord[stratifyOn];
+
+  // We only allow stratification on categorical variables.
+  if (propertyObj.type !== 'categorical')
+    throw new Error(
+      'stratification is only possible to perform on categorical properties',
+    );
+
+  // Make an array of opts if it is not provided as an array.
+  const fixedOpts = Array.isArray(opts)
+    ? opts
+    : new Array(propertyObj.values.length).fill(opts);
+
+  // Make sure it is of correct length.
+  if (fixedOpts.length !== propertyObj.values.length) {
+    throw new Error(
+      'length of provided opts array does not match number of strata',
+    );
+  }
+
+  /*
+   * For each value of the property, run sampleAreaToPoint.
+   * Merge the returning features into a single array.
+   */
+  let features: PointFeature[] = [];
+
+  propertyObj.values.forEach((_v, i) => {
+    const stratumFeatures = layer.collection.features.filter(
+      (f) => f.properties[stratifyOn] === i,
+    );
+
+    const stratumLayer = new Layer(
+      new AreaCollection({features: stratumFeatures}, true),
+      layer.propertyRecord,
+      true,
+    );
+
+    const sampledFeatures = sampleAreaToPoint(stratumLayer, fixedOpts[i])
+      .collection.features;
+
+    features = [...features, ...sampledFeatures];
+  });
+
+  return new Layer(
+    new PointCollection({features}, true),
+    copy(layer.propertyRecord),
+    true,
+  );
+}
+
+export function sampleAreaToAreaStratified(
+  layer: Layer<AreaCollection>,
+  stratifyOn: string,
+  opts:
+    | TsampleAreaToAreaOpts
+    | ISampleAreaFeaturesOnAreasOpts[]
+    | ISampleBeltsOnAreasOpts[],
+): Layer<AreaCollection> {
+  if (!(stratifyOn in layer.propertyRecord))
+    throw new Error(
+      'stratification is not possible as property record does not contain the required property',
+    );
+
+  // The current property object.
+  const propertyObj = layer.propertyRecord[stratifyOn];
+
+  // We only allow stratification on categorical variables.
+  if (propertyObj.type !== 'categorical')
+    throw new Error(
+      'stratification is only possible to perform on categorical properties',
+    );
+
+  // Make an array of opts if it is not provided as an array.
+  const fixedOpts = Array.isArray(opts)
+    ? opts
+    : new Array(propertyObj.values.length).fill(opts);
+
+  // Make sure it is of correct length.
+  if (fixedOpts.length !== propertyObj.values.length) {
+    throw new Error(
+      'length of provided opts array does not match number of strata',
+    );
+  }
+
+  /*
+   * For each value of the property, run sampleAreaToArea.
+   * Merge the returning features into a single array.
+   */
+  let features: AreaFeature[] = [];
+
+  propertyObj.values.forEach((_v, i) => {
+    const stratumFeatures = layer.collection.features.filter(
+      (f) => f.properties[stratifyOn] === i,
+    );
+
+    const stratumLayer = new Layer(
+      new AreaCollection({features: stratumFeatures}, true),
+      layer.propertyRecord,
+      true,
+    );
+
+    const sampledFeatures = sampleAreaToArea(stratumLayer, fixedOpts[i])
+      .collection.features;
+
+    features = [...features, ...sampledFeatures];
+  });
+
+  return new Layer(
+    new AreaCollection({features}, true),
+    copy(layer.propertyRecord),
+    true,
+  );
+}
+
+export function sampleAreaToLineStratified(
+  layer: Layer<AreaCollection>,
+  stratifyOn: string,
+  opts:
+    | TsampleAreaToLineOpts
+    | ISampleLineFeaturesOnAreasOpts[]
+    | ISampleLinesOnAreasOpts[],
+): Layer<LineCollection> {
+  if (!(stratifyOn in layer.propertyRecord))
+    throw new Error(
+      'stratification is not possible as property record does not contain the required property',
+    );
+
+  // The current property object.
+  const propertyObj = layer.propertyRecord[stratifyOn];
+
+  // We only allow stratification on categorical variables.
+  if (propertyObj.type !== 'categorical')
+    throw new Error(
+      'stratification is only possible to perform on categorical properties',
+    );
+
+  // Make an array of opts if it is not provided as an array.
+  const fixedOpts = Array.isArray(opts)
+    ? opts
+    : new Array(propertyObj.values.length).fill(opts);
+
+  // Make sure it is of correct length.
+  if (fixedOpts.length !== propertyObj.values.length) {
+    throw new Error(
+      'length of provided opts array does not match number of strata',
+    );
+  }
+
+  /*
+   * For each value of the property, run sampleAreaToLine.
+   * Merge the returning features into a single array.
+   */
+  let features: LineFeature[] = [];
+
+  propertyObj.values.forEach((_v, i) => {
+    const stratumFeatures = layer.collection.features.filter(
+      (f) => f.properties[stratifyOn] === i,
+    );
+
+    const stratumLayer = new Layer(
+      new AreaCollection({features: stratumFeatures}, true),
+      layer.propertyRecord,
+      true,
+    );
+
+    const sampledFeatures = sampleAreaToLine(stratumLayer, fixedOpts[i])
+      .collection.features;
+
+    features = [...features, ...sampledFeatures];
+  });
+
+  return new Layer(
+    new LineCollection({features}, true),
+    copy(layer.propertyRecord),
+    true,
+  );
+}
