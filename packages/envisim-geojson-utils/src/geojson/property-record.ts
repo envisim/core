cd76a5ed524255551c3e34df4e015f29a9717f65
wpmg <wilmer.prentius@slu.se>
2024-11-19 10:36:17 +0100

    removed commented out code

--- packages/envisim-geojson-utils/src/geojson/property-record.ts
+++ packages/envisim-geojson-utils/src/geojson/property-record.ts
@@ -37,8 +37,6 @@ interface PropertyList {
   [id: string]: Property;
 }
 
-// export type PropertyRecord = Record<string, Property>;
-
 export class PropertyRecord {
   record: SpecialPropertyList & PropertyList = {};
 
@@ -240,73 +238,3 @@ export class PropertyRecord {
     };
   }
 }
-
-// export function createDesignWeightProperty(): NumericalProperty {
-//   return {
-//     id: '_designWeight',
-//     name: 'design weight',
-//     type: 'numerical',
-//   };
-// }
-
-// export function createDistanceProperty(): NumericalProperty {
-//   return {
-//     id: '_distance',
-//     name: 'distance',
-//     type: 'numerical',
-//   };
-// }
-
-// export function createParentProperty(): NumericalProperty {
-//   return {
-//     id: '_parent',
-//     name: 'parent',
-//     type: 'numerical',
-//   };
-// }
-
-// /**
-//  * To be used when converting into classes. Keeps ids as name
-//  */
-// export function createPropertyRecordFromFeature({
-//   properties = {},
-// }: GJ.BaseFeature<GJ.BaseGeometry, any>): PropertyRecord {
-//   const propertyRecord: PropertyRecord = {};
-
-//   Object.entries(properties ?? {}).forEach(([id, prop]) => {
-//     const isSpecialKey = (PropertySpecialKeys as ReadonlyArray<string>).includes(id);
-//     const valueType = typeof prop;
-
-//     if (valueType === 'number') {
-//       propertyRecord[id] = {type: 'numerical', id, name: id};
-//     } else if (isSpecialKey) {
-//       throw new Error(`Property ${prop} is a reserved property and must be a number`);
-//     } else if (valueType === 'string') {
-//       propertyRecord[id] = {type: 'categorical', id, name: id, values: []};
-//     }
-//   });
-
-//   return propertyRecord;
-// }
-
-// /**
-//  * Merges property records into a single property record
-//  *
-//  * @param propertyRecords
-//  * @returns the new property record
-//  */
-// export function mergePropertyRecords(propertyRecords: PropertyRecord[]): PropertyRecord {
-//   const newRecord: PropertyRecord = {};
-
-//   propertyRecords.forEach((record) => {
-//     Object.keys(record).forEach((key) => {
-//       // Check for duplicates
-//       if (Object.hasOwn(newRecord, key)) {
-//         throw new Error('Records contain duplicate identifier.');
-//       }
-//       // Add a copy to new record
-//       newRecord[key] = copy(record[key]);
-//     });
-//   });
-//   return newRecord;
-// }
