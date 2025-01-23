80928947f4116163cafd7b6a0ee27dfbc1f7650f
wpmg <wilmer.prentius@slu.se>
2025-01-23 15:22:33 +0100

    (geosampling) CollectError error collision (#60)
    - Fixed `CollectError` error string collision
    - Added `CollectError` name collision error

--- packages/envisim-geosampling/src/collect/collect-error.ts
+++ packages/envisim-geosampling/src/collect/collect-error.ts
@@ -1,10 +1,11 @@
 export const CollectError = {
   // collect general
   LINE_EXPECTS_AREA_LINE: 'collect-error-line-expects-area-line',
-  POINT_EXPECTS_AREA: 'collect-error-line-expects-area-line',
+  POINT_EXPECTS_AREA: 'collect-error-point-expects-area',
   BASE_COLLECTION_MISSING: 'collect-error-collection-is-missing',
 
   // collect properties
+  PROPERTY_ID_COLLISION: 'collect-error-property-is-missing',
   PROPERTY_MISSING: 'collect-error-property-is-missing',
   PROPERTY_NOT_NUMERICAL: 'collect-error-property-not-numerical',
 } as const;
