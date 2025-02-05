3ed4d4ff972690d8d44c0e7201d59f2e636493cb
wpmg <wilmer.prentius@slu.se>
2025-02-05 16:24:26 +0100

    index catastrophe

--- packages/envisim-geojson-utils/src/intersect/intersect-line-area.ts
+++ packages/envisim-geojson-utils/src/intersect/intersect-line-area.ts
@@ -88,9 +88,9 @@ function lineStringInPolygons(
       ls = [];
     }
 
-    for (let i = 1; i < vals.length - 1; i++) {
-      ls.push(seg.position(vals[i]));
-      if (i % 2 === 0) {
+    for (let j = 1; j < vals.length - 1; j++) {
+      ls.push(seg.position(vals[j]));
+      if (j % 2 === 0) {
         mls.push(ls);
         ls = [];
       }
