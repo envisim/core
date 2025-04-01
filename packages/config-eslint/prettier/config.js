69b37d4a824159aa9650ca1fe7c2772ef51b7a3a
wpmg <wilmer.prentius@slu.se>
2025-04-01 10:26:11 +0200

    (config) prettier config bugfix (#87)

--- packages/config-eslint/prettier/config.js
+++ packages/config-eslint/prettier/config.js
@@ -4,7 +4,7 @@
 export const defaultConfigObject = {
   printWidth: 100,
   quoteProps: "consistent",
-  importOrder: ["^@types/(.*)$", "<THIRD_PARTY_MODULES>", "^@envisim/(.*)$", "^[$]", , "^[./]"],
+  importOrder: ["^@types/(.*)$", "<THIRD_PARTY_MODULES>", "^@envisim/(.*)$", "^[$]", "^[./]"],
 };
 
 /**
