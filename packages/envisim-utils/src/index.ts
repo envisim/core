2403f60a81f536287261b69b83d4897f54a4d630
wpmg <wilmer.prentius@slu.se>
2025-05-28 13:49:17 +0200

    Remove throwRangeError

--- packages/envisim-utils/src/index.ts
+++ packages/envisim-utils/src/index.ts
@@ -11,7 +11,6 @@ export {
 export { inClosedInterval, inOpenInterval } from "./numeric.js";
 export { reducedRowEchelonForm } from "./reducedRowEchelonForm.js";
 export { swap } from "./swap.js";
-export { throwRangeError } from "./throw.js"; // REMOVE
 
 // Types
 export type { OptionalParam } from "./types.js";
